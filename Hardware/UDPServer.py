b73190fb14f81da26666f7b18e0922526a4e6dce
Shijia Chen <35570690+sjChen15@users.noreply.github.com>
2023-07-07 12:59:37 -0400

    working UDP server

---
+++ Hardware/UDPServer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+#Ref: https://stackoverflow.com/questions/64642122/how-to-send-real-time-sensor-data-to-pc-from-raspberry-pi-zero
+import socket
+from struct import unpack
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind the socket to the port
+host, port = '0.0.0.0', 65000
+server_address = (host, port)
+
+print(f'Starting UDP server on {host} port {port}')
+sock.bind(server_address)
+
+while True:
+    # Wait for message
+    message, address = sock.recvfrom(4096)
+
+    print(f'Received {len(message)} bytes:')
+    x, y, z = unpack('3f', message)
+    print(f'X: {x}, Y: {y}, Z: {z}')

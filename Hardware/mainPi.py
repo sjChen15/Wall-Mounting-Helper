93f83e44f5f979e82c6168cbdad884c1ee37f497
sjChen15 <shijiajchen@gmail.com>
2023-07-16 13:39:50 -0400

    working blank pygame

--- Hardware/mainPi.py
+++ Hardware/mainPi.py
@@ -7,26 +7,23 @@ pygame.init()
 #Create Window with custom title
 pygame.display.set_caption("Wall Mounting Helper")
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+#screen = pygame.display.set_mode((400,200))
 WIDTH, HEIGHT = screen.get_size()
 CENTER_X, CENTER_Y = WIDTH // 2, HEIGHT // 2
 
 #Test display
 x,y,z = sensorProcessingPi.getSensorData()
-text = FONT.render(f'{x} {y} {z}', True, BLACK)
+font = pygame.font.Font(pygame.font.get_default_font(), 32)
+text = font.render(f'{x} {y} {z}', True, BLACK)
 textRect = text.get_rect()
 textRect.center = (CENTER_X, CENTER_Y - 200)
 
 #Cross in center
-vert_rect = pygame.Rect(CENTER_X-10,CENTER_Y-50,20,100)
-hori_rect = pygame.Rect(CENTER_X-50,CENTER_X-10,100,20)
+vert_rect = pygame.Rect(CENTER_X-2,CENTER_Y-8,4,16)
+hori_rect = pygame.Rect(CENTER_X-8,CENTER_Y-2,16,4)
 
 run = True
 while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            run = False
-
     #Fill display screen
     screen.fill(WHITE) #White background
 
@@ -36,4 +33,13 @@ while run:
     #Crosshair
     pygame.draw.rect(screen, RED, vert_rect)
     pygame.draw.rect(screen, RED, hori_rect)
+    pygame.display.update()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            pygame.quit()
+            run = False
     
--- Hardware/pygameConsts.py
+++ Hardware/pygameConsts.py
@@ -1,9 +1,4 @@
-import pygame
-
 #Colours
 WHITE  = (255,255,255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
-
-#Pygame Values
-FONT = pygame.font.Font('freesansbold.ttf', 32)

8aede106932956479f11d744cc56d839ea31b334
Shijia Chen <35570690+sjChen15@users.noreply.github.com>
2023-07-25 14:41:36 -0400

    connections work

--- Hardware/mainPC.py
+++ Hardware/mainPC.py
@@ -3,21 +3,26 @@ import TCPClient
 import UDPServer
 import keyboard
 
-IP = '10.0.0.47'
+PC_IP = '10.0.0.47'
+PI_IP = '10.0.0.86'
 TCP_CAMERA_PORT = 12345 
 TCP_SKEW_PORT = 9999
 UDP_PORT = 65000
 #tcp_server = TCPServer.ServerSocket(TCP_IP, TCP_PORT)
-sensors_udp_server = UDPServer.ServerSocket(IP, UDP_PORT)
-skewed_image_tcp_client = TCPClient.ClientSocket(IP, TCP_SKEW_PORT)
+sensors_udp_server = UDPServer.ServerSocket(PC_IP, UDP_PORT)
+skewed_image_tcp_client = TCPClient.ClientSocket(PI_IP, TCP_SKEW_PORT)
 skewed_image_filename = "imgs_to_send/squink.png"
+count = 1
 try:
     while True:
-        sensors_udp_server.waitForMessage()
-
-        if keyboard.is_pressed('space'): #send picture on space
+        if count%5 == 0: #send picture on space
             skewed_image_tcp_client.sendImage(skewed_image_filename)
+            count = 0
             break
+
+        sensors_udp_server.waitForMessage()
+        
+        count += 1
 
 except Exception as e:
     print(e)

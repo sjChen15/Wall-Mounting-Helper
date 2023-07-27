73dea3a2e047ff0f8e5a033ca2209579e5ce04b3
Shijia Chen <35570690+sjChen15@users.noreply.github.com>
2023-07-27 16:01:58 -0400

    close sockets on interrupt

--- Hardware/image_receive_process_pc.py
+++ Hardware/image_receive_process_pc.py
@@ -8,13 +8,10 @@ ip = "0.0.0.0"
 # Bind the socket to a specific port
 sensor_server = TCPServer.ServerSocket(ip,port)
 save_path = ("C:/Users/shiji/OneDrive/Documents/Wall-Mounting-Helper/Hardware/imgs_received/pi_cam_img.jpg")  # Specify the path to save the received image
-img_num = 0
 
-while True:
-    img_num +=1
-    sensor_server.socketOpenAndReceiveImage(save_path)
+try:
+    while True:
+        sensor_server.socketOpenAndReceiveImage(save_path)
 
-    if img_num > 10:
-        break
-
-sensor_server.closeSocket()
+except KeyboardInterrupt:
+    sensor_server.closeSocket()
--- Hardware/image_receive_process_pi.py
+++ Hardware/image_receive_process_pi.py
@@ -9,8 +9,9 @@ ip = "0.0.0.0"
 sensor_server = TCPServer.ServerSocket(ip,port)
 save_path = ("imgs_received/received_image.jpg")  # Specify the path to save the received image
 
-while True:
-    sensor_server.socketOpenAndReceiveImage(save_path)
+try:
+    while True:
+        sensor_server.socketOpenAndReceiveImage(save_path)
 
-
-sensor_server.closeSocket()
+except KeyboardInterrupt:
+    sensor_server.closeSocket()
